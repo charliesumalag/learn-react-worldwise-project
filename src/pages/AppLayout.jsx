d0ce71568dc1ed55ae9d38271778b3a9ed9dc41c
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-08-11 14:28:28 +0800

    sidebar and map

--- src/pages/AppLayout.jsx
+++ src/pages/AppLayout.jsx
@@ -2,15 +2,15 @@
 // import Sidebar from "../components/Sidebar";
 // import User from "../components/User";
 
-import AppNav from "../components/AppNav";
+import Map from "../components/Map";
+import Sidebar from "../components/Sidebar";
 import styles from "./AppLayout.module.css";
 
 function AppLayout() {
   return (
     <div className={styles.app}>
-      <AppNav />
-      <p>App</p>
-      this is applayout
+      <Sidebar />
+      <Map />
     </div>
   );
 }
