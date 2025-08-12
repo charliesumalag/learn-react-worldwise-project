de688e3c179163ff2939cb0bf9a86ecc41dc04f9
Charlie Sumalag <charliesumalag444@yahoo.com.ph>
2025-08-12 12:27:10 +0800

    implementing cities and countries

--- src/App.jsx
+++ src/App.jsx
@@ -5,17 +5,50 @@ import Homepage from "./pages/Homepage";
 import PageNotFound from "./pages/PageNotFound";
 import AppLayout from "./pages/AppLayout";
 import Login from "./pages/Login";
+import CityList from "./components/CityList";
+import CountryList from "./components/CountryList";
+import { useEffect, useState } from "react";
+const BASE_URL = 'http://localhost:9000';
 
 const App = () => {
+  const [cities, setCities] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    async function fetchCities() {
+      try {
+        setIsLoading(true);
+        const res = await fetch(`${BASE_URL}/cities`);
+        const data = await res.json();
+        setCities(data);
+      }catch {
+        alert('Theres was an error loading data....')
+      } finally {
+        setIsLoading(false);
+      }
+    }
+
+    // calling the function
+    fetchCities();
+  }, []);
+
+
+
+
   return (
 
     <BrowserRouter>
       <Routes>
-          <Route path='/' element={ <Homepage />} />
+          <Route index element={ <Homepage />} />
           <Route path="product" element={ <Product /> } />
           <Route path="pricing" element={ <Pricing /> } />
           <Route path="login" element={ <Login /> } />
-          <Route path="app" element={ <AppLayout /> } />
+          <Route path="app" element={ <AppLayout /> }>
+            <Route index element={ <CityList cities={cities} isLoading={isLoading}  />} />
+            <Route path="cities" element={ <CityList cities={cities} isLoading={isLoading}  /> } />
+            <Route path="countries" element={ <CountryList cities={cities} isLoading={isLoading} /> } />
+            <Route path="form" element={ <p>Form</p>} />
+          </Route>
           <Route path="*" element={ <PageNotFound /> } />
       </Routes>
     </BrowserRouter>
---
+++ src/components/CountryList.jsx
@@ -0,0 +1,18 @@
+import styles from "./CountryList.module.css";
+import Spinner   from "./Spinner";
+import CityItem from "./CityItem";
+import Message from "./Message";
+
+const CountryList = ({ cities,  isLoading}) => {
+
+  if(isLoading) return <Spinner />;
+
+  if(!cities.length) return <Message message='Add your firstcity by clicking on a city on the map' />
+  return (
+    <ul className={styles.countriesList}>
+      {cities.map((city) => (<CityItem city={city} key={city.id} />) )}
+    </ul>
+  )
+}
+
+export default CountryList
